b709874fb6b8848ce5f0b4f2c55add0e0f1066e7
andrexzy <amdresilva2020@gmail.com>
2024-05-20 15:49:56 -0300

    Create aula3.py

---
+++ aula3.py
@@ -0,0 +1,42 @@
+# Variavel inteira (INT)
+idade = 25
+
+# Variavel de ponto flutuante (FLOAT)
+altura = 1.70
+
+# variavel de texto (STRING)
+nome = "kaique"
+
+# Variavel booleana (BOOL)
+tem_carro = False
+
+# Exibir informaçoes
+print("nome: ", nome)
+
+print("idade: ", idade)
+
+print ("altura ", altura)
+
+print ("tem_carro:", tem_carro)
+
+ano=1999
+peso=70.35
+apelido="carlinhos123"
+mora_de_aluguel=True
+
+print("apelido: ", apelido)
+print("ano: ", ano)
+print ("peso: ", peso)
+print ("mora de aluguel?", mora_de_aluguel)
+
+idade_nova=idade + 20
+print (idade_nova)
+
+idade_nova=idade_nova-10
+print(idade_nova)
+
+idade_nova = idade_nova*15
+print(idade_nova)
+
+idade_nova=idade_nova/30
+print(idade_nova)
